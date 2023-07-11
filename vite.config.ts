f95dde9bb0bfecb594d4ef77e3316acd5078def7
nad279444 <nardfrimpong@gmail.com>
2023-07-11 10:34:10 +0000

    deploy to github-pages

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,9 @@ import path from 'path';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/',
   resolve:{
     alias: [{find : "@", replacement : path.resolve(__dirname,"src")}]
   }
+ 
 })
